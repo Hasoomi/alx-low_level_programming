bcc931d1e2ea9bd90a1b96bd10531d01f2b114db
Hasoomi <107178001+Hasoomi@users.noreply.github.com>
2022-07-11 22:49:16 +0300

    Add files via upload

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "mian.h"
+
+/**
+* swap_int -> ginen two integers swap the values they are holding
+* @a: parameter 1
+* @b: parameter 2
+*/
+void swap_int(int *a, int *b)
+{
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length - 1;
+	for (i = 0; i < length1 / 2; i++)
+	{
+	tmp = s[i];
+	s[i] = s[length2];
+	s[length2] = tmp;
+	length2 -= 1;
+	}
+}
